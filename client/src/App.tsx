7dbc9b2ee691a97af61dba97886bcd8905500524
Dmitry Kolesnichenko <dmitriykol@gmail.com>
2023-03-12 14:43:42 +0400

    minor refactor changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,6 @@ import { FC, useState, useRef, useEffect } from 'react'
 import cn from 'classnames'
 import bot from './assets/bot.svg'
 import user from './assets/user.svg'
-import send from './assets/send.svg'
 import Loader from './Loader'
 import axios from 'axios'
 
@@ -15,27 +14,36 @@ interface data {
 }
 
 const App: FC = () => {
-  const [searchReq, setSearchReq] = useState('')
-  // const [response, setResponse] = useState('')
+  const [searchReq, setSearchReq] = useState<string>('')
   const [dataArr, setDataArr] = useState<data[]>([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
 
+  const chatContainerRef = useRef<HTMLDivElement>(null)
   const inputRef = useRef<HTMLInputElement>(null)
 
-  const generateUniqueId = () => {
+  const handleScroll = (): void => {
+    chatContainerRef.current?.scrollTo({
+      top: chatContainerRef.current.scrollHeight,
+      behavior: 'smooth',
+    })
+  }
+
+  const handleFocus = (): void => {
+    inputRef.current?.focus()
+  }
+
+  const generateUniqueId = (): string => {
     const timestamp = Date.now()
     const randomNumber = Math.random()
     const hexadecimalString = randomNumber.toString(16)
-
     return `id-${timestamp}-${hexadecimalString}`
   }
 
-  const getData = async () => {
+  const getData = async (): Promise<void> => {
     try {
       setIsLoading(true)
       const prompt = searchReq
-      setSearchReq('')
-      const response = await axios<any>({
+      const response = await axios<{ bot: string }>({
         method: 'POST',
         url: 'http://localhost:5000',
         headers: {
@@ -47,38 +55,46 @@ const App: FC = () => {
       })
       const { data } = response
       const parsedData = data.bot.trim()
-      const arr = dataArr;
+      const arr = dataArr
       arr[arr.length - 1].value = parsedData
-      setIsLoading(false)
       setDataArr(arr)
+      setSearchReq('')
+      setTimeout(() => {
+        handleScroll()
+        handleFocus()
+      }, 100);
     } catch (err) {
+      console.log(err)
+    } finally {
       setIsLoading(false)
-      console.log(err);
     }
   }
 
-  const handleSubmit = async (e?: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e?: React.FormEvent<HTMLFormElement>): Promise<void> => {
     e?.preventDefault()
+    if (!searchReq || isLoading) return
     const uniqueId = generateUniqueId()
     setDataArr([
       ...dataArr,
       { isAI: false, value: searchReq },
       { isAI: true, value: '', uniqueId },
     ])
-    inputRef.current?.focus()
   }
 
   useEffect(() => {
-    if (dataArr && searchReq) getData()
+    if (dataArr && searchReq) {
+      handleScroll()
+      getData()
+    }
   }, [dataArr])
 
   useEffect(() => {
-    inputRef.current?.focus()
+    handleFocus()
   }, [])
 
   return (
     <div className="outer">
-      <div className="chat-container">
+      <div className="chat-container" ref={chatContainerRef}>
         {dataArr.map((el, i) => {
           return (
             <div key={i} className={cn('wrapper', el.isAI && 'ai')}>
@@ -98,15 +114,20 @@ const App: FC = () => {
           )
         })}
       </div>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit} className="form">
         <input
           name="prompt"
           value={searchReq}
           onChange={(e) => setSearchReq(e.target.value)}
+          className="input"
           ref={inputRef}
+          disabled={isLoading}
         ></input>
-        <button type="submit">
-          <img src={send} alt="" />
+        <button
+          type="submit"
+          className={cn('submitBtn', isLoading && 'disabled')}
+        >
+          Submit
         </button>
       </form>
     </div>
